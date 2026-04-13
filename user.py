f7250cb3b0abe7b8f0d9e975756b967585708791
Damien <damieno2006@gmail.com>
2026-04-13 15:52:49 -0400

    Add basic attributes for user

--- user.py
+++ user.py
@@ -3,11 +3,28 @@ This program will define the user class for the arcade project of ECE3822
 
 Revision History:
     (DO) 04/10/2026 Create initial program
+    (ALL) 04/13/2026 Revise user class to gave all attributes
 
 """
 
+# import all datastructures
+
+from datastructures.sparse_matrix import SparseMatrix
+
 # class storing user information
 class user:
-    def __init__(self):
+    def __init__(self, name):
+
+        # set username for user so others can find them
+        self.name = name
+
+        # give them an id based on hash function of name
         self.__id = None
-        self._playHistory = None
+
+        # id is bucket index
+
+        # store their individual play history
+        self.History = None
+
+        # store sparse matrix to get recommendations
+        self.__game_recommendation = SparseMatrix()
