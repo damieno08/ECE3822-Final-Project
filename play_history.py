5736a07d5a0977f62ddc3e590eafabe2571801be
SITRO05 <sitrom@icloud.com>
2026-04-13 16:28:32 -0400

    Initial commit play history

---
+++ play_history.py
@@ -0,0 +1,12 @@
+from history import History
+
+class Play_history(History):
+    def __init__(self, time):
+
+        # Inheret the history class
+        super().__init__(self)
+
+        # Save time the message was sent
+        self.time = time
+        
+        # add more data related to play history...
