c1fd32b95c482922c950ebec61997110e04c16e9
RichardLinEngineer <tut03727@temple.edu>
2026-04-14 17:06:18 -0400

    create intial program

--- user_interaction/play_history.py
+++ user_interaction/play_history.py
@@ -1,10 +1,18 @@
+"""
+This program will define the playhistory class for the arcade project of ECE3822
+
+Revision History:
+    (RL) 04/13/2026 Create initial program
+
+"""
+
 from user_interaction.history import History
 
 class Play_history(History):
     def __init__(self, time):
 
         # Inheret the history class
-        super().__init__(self)
+        super().__init__()
 
         # Save time the message was sent
         self.time = time
