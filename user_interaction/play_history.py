695c9a1d32cd285bb14492dc03d7fc42d2dd3193
RichardLinEngineer <tut03727@temple.edu>
2026-04-22 19:26:01 -0400

    play history

--- user_interaction/play_history.py
+++ user_interaction/play_history.py
@@ -1,3 +1,12 @@
+"""
+This program will define the play_history class for the arcade project of ECE3822
+
+Revision History:
+    (ALL) 04/10/2026 Create initial program
+    (RL)  04/22/2026 Create some methods
+"""
+
+from game_interaction.game_session import GameSession
 from user_interaction.history import History
 
 class Play_history(History):
@@ -12,4 +21,31 @@ class Play_history(History):
         # add more data related to play history...
 
     def pop(self):
+        """
+        Gives the most recent session
+        """
         return self._history.pop()
+    
+    def size(self):
+        """
+        Total number of games played
+        """
+        return self._history.size()
+
+    def get_total_time(self):
+        """
+        Total playtime
+        """
+        total = 0
+        sessions = self._history.get_array()
+
+        for session in sessions:
+            total += session.get_time_played().total_seconds()
+        
+        return total
+
+    def get_history(self, idx=-1):
+        """
+        Get session by index
+        """
+        return self._history.peek(idx)
