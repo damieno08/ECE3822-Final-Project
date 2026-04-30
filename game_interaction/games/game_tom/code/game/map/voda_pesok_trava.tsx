9d02a97aa7a1afa056a81073ab953be11af1695c
LipskiT-code <227703040+LipskiT-code@users.noreply.github.com>
2026-04-30 09:38:08 -0400

    Updated graphics for Tom's game.

---
+++ game_interaction/games/game_tom/code/game/map/voda_pesok_trava.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="voda_pesok_trava" tilewidth="64" tileheight="64" tilecount="4" columns="2">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../../../../../../../../../Labs/lab-07-tom-lipski-1/code/game/map/voda_pesok_trava.png" width="160" height="144"/>
+</tileset>
--- game_interaction/games/game_tom/code/game/subcharacter.py
+++ game_interaction/games/game_tom/code/game/subcharacter.py
@@ -10,159 +10,42 @@ from game_interaction.games.game_tom.code.game.character import Character
 from game_interaction.games.game_tom.code.game.item import Weapon
 from datetime import datetime
 
-# ================================ Default Characters ===================================================
-
-# class Character1(Character):
-#     """Cleric - Healing specialist"""
-#     def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
-#         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-#         self.character_name = "Cleric"
-#         self.hp, self.max_hp = 120, 120
-#         self.attack, self.defense = 8, 7
-        
-#         # Load image for remote players or as fallback
-#         try:
-#             self.image = pygame.image.load('../../graphics/characters/cleric/down/frame_000.png').convert_alpha()
-#             self.rect = self.image.get_rect(topleft=pos)
-#             self.hitbox = self.rect.inflate(0, -26)
-#         except:
-#             pass
-            
-#         # Load animations for local player
-#         if is_local:
-#             self.import_player_assets(animate=True)
-    
-#     @staticmethod
-#     def get_display_name():
-#         return "Cleric"
-    
-#     @staticmethod
-#     def get_description():
-#         return "Healing specialist with high HP"
-    
-#     @staticmethod
-#     def get_preview_image():
-#         return '../../graphics/characters/cleric/down/frame_000.png'
-
-
-# class Character2(Character):
-#     """Hobbit - Sneaky and fast"""
-#     def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
-#         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-#         self.character_name = "Hobbit"
-#         self.hp, self.max_hp = 80, 80
-#         self.attack, self.defense = 12, 4
-#         self.speed = 6  # Faster than others
-        
-#         try:
-#             self.image = pygame.image.load('../../graphics/characters/hobbit/down/frame_000.png').convert_alpha()
-#             self.rect = self.image.get_rect(topleft=pos)
-#             self.hitbox = self.rect.inflate(0, -26)
-#         except:
-#             pass
-            
-#         if is_local:
-#             self.import_player_assets(animate=True)
-    
-#     @staticmethod
-#     def get_display_name():
-#         return "Hobbit"
-    
-#     @staticmethod
-#     def get_description():
-#         return "Fast and sneaky"
-    
-#     @staticmethod
-#     def get_preview_image():
-#         return '../../graphics/characters/hobbit/down/frame_000.png'
-
-
-# class Character3(Character):
-#     """Thief - High attack, low defense"""
-#     def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
-#         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-#         self.character_name = "Thief"
-#         self.hp, self.max_hp = 90, 90
-#         self.attack, self.defense = 15, 3
-        
-#         try:
-#             self.image = pygame.image.load('../../graphics/characters/thief/down/frame_000.png').convert_alpha()
-#             self.rect = self.image.get_rect(topleft=pos)
-#             self.hitbox = self.rect.inflate(0, -26)
-#         except:
-#             pass
-            
-#         if is_local:
-#             self.import_player_assets(animate=True)
-    
-#     @staticmethod
-#     def get_display_name():
-#         return "Thief"
-    
-#     @staticmethod
-#     def get_description():
-#         return "Glass cannon - high attack"
-    
-#     @staticmethod
-#     def get_preview_image():
-#         return '../../graphics/characters/thief/down/frame_000.png'
-
-
-# class Character4(Character):
-#     """Wizard - Magical powerhouse"""
-#     def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
-#         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-#         self.character_name = "Wizard"
-#         self.hp, self.max_hp = 100, 100
-#         self.attack, self.defense = 10, 5
-        
-#         try:
-#             self.image = pygame.image.load('../../graphics/characters/wizard/down/frame_000.png').convert_alpha()
-#             self.rect = self.image.get_rect(topleft=pos)
-#             self.hitbox = self.rect.inflate(0, -26)
-#         except:
-#             pass
-            
-#         if is_local:
-#             self.import_player_assets(animate=True)
-    
-#     @staticmethod
-#     def get_display_name():
-#         return "Wizard"
-    
-#     @staticmethod
-#     def get_description():
-#         return "Balanced mage with special abilities"
-    
-#     @staticmethod
-#     def get_preview_image():
-#         return '../../graphics/characters/wizard/down/frame_000.png'
-
-
-
-# ===================================== Custom Characters ==============================================================================
+start_path = str(sys.path[0])
 
 
 class Character1(Character):
     """Class Name: Fire Class"""
-    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
+    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True, test=False):
         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
+
+        # Checks if we are running in test mode, if we are then just make the image a surface.
+        # if we aren't, use the real sprite
+        #
+        if test:
+            self.image = pygame.Surface((64, 64))
+        else:
+            self.image = pygame.image.load(start_path + '/game_interaction/games/game_tom/graphics/characters/Salamus.png').convert_alpha()
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
+        
+            
         self.character_name = "Salamus"
         self.hp, self.max_hp = 100, 100
         self.attack, self.defense = 10, 10
         self.speed = 6
         
-        # Load image for remote players or as fallback
-        try:
-            self.image = pygame.image.load('../../graphics/characters/Salamus.png').convert_alpha()
-            self.rect = self.image.get_rect(topleft=pos)
-            self.hitbox = self.rect.inflate(0, -26)
-        except:
-            pass
+        # # Load image for remote players or as fallback
+        # try:
+        #     self.image = pygame.image.load('/game_interaction/games/game_tom/graphics/characters/Salamus.png').convert_alpha()
+        #     self.rect = self.image.get_rect(topleft=pos)
+        #     self.hitbox = self.rect.inflate(0, -26)
+        # except:
+        #     pass
             
-        # Load animations for local player
-        if is_local:
-            self.import_player_assets(animate=True)
+        # # Load animations for local player
+        # if is_local:
+        #     self.import_player_assets(animate=True)
     
     @staticmethod
     def get_display_name():
@@ -174,31 +57,44 @@ class Character1(Character):
     
     @staticmethod
     def get_preview_image():
-        return '../../graphics/characters/Salamus.png'
+        return start_path + "/game_interaction/games/game_tom/graphics/characters/Salamus.png"
 
 
 class Water(Character):
     """Class Name: Water Class"""
-    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
+    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True, test=False):
         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-        self.character_name = "Olive"
+
+        # Checks if we are running in test mode, if we are then just make the image a surface.
+        # if we aren't, use the real sprite
+        #
+        if test:
+            self.image = pygame.Surface((64, 64))
+        else:
+            self.image = pygame.image.load(start_path + '/game_interaction/games/game_tom/graphics/characters/Olive.png').convert_alpha()
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
+
+        
+        self.character_name = "Olivius"
         self.hp, self.max_hp = 100, 100
         self.attack, self.defense = 10, 10
         self.speed = 4  # Faster than others
         
-        try:
-            self.image = pygame.image.load('../../graphics/characters/Olive.png').convert_alpha()
-            self.rect = self.image.get_rect(topleft=pos)
-            self.hitbox = self.rect.inflate(0, -26)
-        except:
-            pass
+        # try:
+        #     self.image = pygame.image.load('../../graphics/characters/Olive.png').convert_alpha()
+        #     self.rect = self.image.get_rect(topleft=pos)
+        #     self.hitbox = self.rect.inflate(0, -26)
+        # except:
+        #     pass
             
-        if is_local:
-            self.import_player_assets(animate=True)
+        # if is_local:
+        #     self.import_player_assets(animate=True)
     
     @staticmethod
     def get_display_name():
-        return "Olive"
+        return "Olivius"
     
     @staticmethod
     def get_description():
@@ -206,30 +102,42 @@ class Water(Character):
     
     @staticmethod
     def get_preview_image():
-        return '../../graphics/characters/Olive.png'
+        return start_path + "/game_interaction/games/game_tom/graphics/characters/Olive.png"
 
 
 class Earth(Character):
     """Class Name: Earth Class"""
-    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
+    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True, test=False):
         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-        self.character_name = "Wendl"
+
+        # Checks if we are running in test mode, if we are then just make the image a surface.
+        # if we aren't, use the real sprite
+        #
+        if test:
+            self.image = pygame.Surface((64, 64))
+        else:
+            self.image = pygame.image.load(start_path + '/game_interaction/games/game_tom/graphics/characters/Wendl.png').convert_alpha()
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)        
+        
+        self.character_name = "Wendius"
         self.hp, self.max_hp = 100, 100
         self.attack, self.defense = 5, 5
         
-        try:
-            self.image = pygame.image.load('../../graphics/characters/Wendl.png').convert_alpha()
-            self.rect = self.image.get_rect(topleft=pos)
-            self.hitbox = self.rect.inflate(0, -26)
-        except:
-            pass
+        # try:
+        #     self.image = pygame.image.load('../../graphics/characters/Wendl.png').convert_alpha()
+        #     self.rect = self.image.get_rect(topleft=pos)
+        #     self.hitbox = self.rect.inflate(0, -26)
+        # except:
+        #     pass
             
-        if is_local:
-            self.import_player_assets(animate=True)
+        # if is_local:
+        #     self.import_player_assets(animate=True)
     
     @staticmethod
     def get_display_name():
-        return "Wendl"
+        return "Wendius"
     
     @staticmethod
     def get_description():
@@ -237,31 +145,43 @@ class Earth(Character):
     
     @staticmethod
     def get_preview_image():
-        return '../../graphics/characters/Wendl.png'
+        return start_path + "/game_interaction/games/game_tom/graphics/characters/Wendl.png"
 
 
 class Air(Character):
     """Class Name: Air"""
-    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True):
+    def __init__(self, pos, groups, obstacle_sprites, player_id=None, is_local=True, test=False):
         super().__init__(pos, groups, obstacle_sprites, player_id, is_local)
-        self.character_name = "Ernesto"
+
+                # Checks if we are running in test mode, if we are then just make the image a surface.
+        # if we aren't, use the real sprite
+        #
+        if test:
+            self.image = pygame.Surface((64, 64))
+        else:
+            self.image = pygame.image.load(start_path + '/game_interaction/games/game_tom/graphics/characters/Ernesto.png').convert_alpha()
+
+        self.rect = self.image.get_rect(topleft=pos)
+        self.hitbox = self.rect.inflate(0, -26)
+        
+        self.character_name = "Erneus"
         self.hp, self.max_hp = 100, 100
         self.attack, self.defense = 4, 6
         self.speed = 30
         
-        try:
-            self.image = pygame.image.load('../../graphics/characters/Ernesto.PNG').convert_alpha()
-            self.rect = self.image.get_rect(topleft=pos)
-            self.hitbox = self.rect.inflate(0, -26)
-        except:
-            pass
+        # try:
+        #     self.image = pygame.image.load('../../graphics/characters/Ernesto.PNG').convert_alpha()
+        #     self.rect = self.image.get_rect(topleft=pos)
+        #     self.hitbox = self.rect.inflate(0, -26)
+        # except:
+        #     pass
             
-        if is_local:
-            self.import_player_assets(animate=True)
+        # if is_local:
+        #     self.import_player_assets(animate=True)
     
     @staticmethod
     def get_display_name():
-        return "Ernesto"
+        return "Erneus"
     
     @staticmethod
     def get_description():
@@ -269,7 +189,7 @@ class Air(Character):
     
     @staticmethod
     def get_preview_image():
-        return '../../graphics/characters/Ernesto.PNG'
+        return start_path + "/game_interaction/games/game_tom/graphics/characters/Ernesto.png"
 
 def get_all_character_classes():
     """Auto-discover all character classes"""
@@ -278,3 +198,4 @@ def get_all_character_classes():
         if cls.__name__ != 'Character' or cls.__name__.startswith('Character'):
             character_classes.append(cls)
     return character_classes
+
